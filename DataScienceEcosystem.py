1a7f2be6c70db65839b812f68eb0e1c9fdc59a03
Braxton Dyer <142927865+brax1227@users.noreply.github.com>
2024-05-28 10:30:48 -0500

    Add files via upload
    This is a minor assignment for my Data Science Certification through IBM

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objectives:__
+#    * Make an ordered list
+#    * Make an unordered list
+#    * Do simple arithmetic
+#    * List popular lnaguages used in Data Science
+#    * Make a Table
+
+# ## Author
+# Braxton Dyer
+# 
+
+# Some of the popular languages that Data Scientists use are: 
+# 1. Python
+# 2. SQL
+# 3. Java
+# 4. Javascript
+# 5. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Matplotlib
+# 3. Pandas
+
+# | Data Science Tools |
+# |----|
+# | Jupyter Notebook |
+# | IBM Watson Studio |
+# | Visual Code Studio |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+#This is a simple arithmetic expression to multiply and then add integers.
+(3*4)+5
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by dividing by 60.
+200 / 60 
+
+
+# 
